cdc6d260c90c2757a1e1f05be6155f2d788a0a08
Ilgrand <demarzoad@libero.it>
2023-05-31 17:20:05 +0200

    Movimento sull'asse X

--- main.py
+++ main.py
@@ -1,13 +1,35 @@
 import XInput as input
-import pyautogui
+import pyautogui as pya
 import os
 from time import sleep
+import keyboard
 
+#N of controller (MAX 4 Controllers connected)
+print(input.get_connected()) # Write in N the index of the returned touple
+N = 0
 
+#Thumbs states
+    #Left Thumb: [0]
+    #Right Thumb: [1]
+LEFT_THUMB = 0
+RIGHT_THUMB = 1
+XVALUE = 0
+YVALUE = 1
 
-
-
+    
 while True:
-    print(input.get_button_values(input.get_state(0))["A"])
+    currentx,currenty = pya.position()
+
+    while(input.get_thumb_values(input.get_state(0))[LEFT_THUMB][XVALUE] > 0):
+        currentx *= 11100
+        pya.moveTo(currentx, currenty,0.2)
+
+    while(input.get_thumb_values(input.get_state(0))[LEFT_THUMB][XVALUE] < 0):
+        currentx -= 10
+        pya.moveTo(currentx, currenty,1)
+
+    
+
+    
 
 
